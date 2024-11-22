95eb3cc69000918d71add6e90556ded8fd3e68d7
zypzyp69 <161214966+zypzyp69@users.noreply.github.com>
2024-11-23 00:20:21 +0800

    Delete finals1/core/data.sql

--- finals1/core/data.sql
+++
@@ -1,9 +0,0 @@
-create table veterinary (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	gender VARCHAR(255),
-	specialization VARCHAR(255),
-	years_of_experience VARCHAR(255),
-	date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
